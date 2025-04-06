c1a214807d67d156e6cff9b9333fa7e39dd98deb
PabloChavarria <pablocha18@gmail.com>
2025-04-06 18:44:18 -0500

    commit de prueba a main

--- class_2/exercises/exercise_5.py
+++ class_2/exercises/exercise_5.py
@@ -3,4 +3,7 @@ Write a program that takes a dictionary with numerical values and calculates
 the average of those values. The dictionary keys are student names, and the values
  are their respective scores.
 
+
 Concepts Covered: Dictionaries, for loops, values(), sum(), len(), print"""
+
+
