fda505e54cbb6af566756c36aa41eb4cbdec98f1
jfrenzac <jfrenzac@gmail.com>
2025-04-16 10:35:02 -0500

    Update last exercises from class 2

--- class_2/exercises/exercise_7.py
+++ class_2/exercises/exercise_7.py
@@ -5,6 +5,21 @@ Add each number to a list.
 Stop asking when the user types "done".
 Then, print the list of numbers and their total sum."""
 
+"""Goal: Use a while loop to collect input and store it in a list."""
+
 # Your code here
 
-"""Goal: Use a while loop to collect input and store it in a list."""
+
+def list_builder():
+    numbers = []
+    while True:
+        number = input("Ingrese un número entero: ")
+        if number == "done":
+            break
+        numbers.append(int(number))
+
+    return f"The sum of all the given numbers is: {sum(numbers)}"
+
+
+if __name__ == "__main__":
+    print(list_builder())
--- class_2/exercises/exercise_8.py
+++ class_2/exercises/exercise_8.py
@@ -4,10 +4,21 @@ Given two lists: one of names and one of ages.
 Print each person with their number (starting from 1) and age,
 using zip() and enumerate()."""
 
-names = ["Alice", "Bob", "Charlie"]
-ages = [25, 30, 22]
-
 # Your code here
+
+
+def combine_list(names: list, ages: list):
+    output = ""
+    for i, name_age in enumerate(zip(names, ages), start=1):
+        output += f"{i}. {name_age[0]} is {name_age[1]} years old\n"
+    return output
+
+
+if __name__ == "__main__":
+    names = ["Alice", "Bob", "Charlie"]
+    ages = [25, 30, 22]
+    print(combine_list(names, ages))
+
 
 # Example Output:
 
