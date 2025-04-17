74b07c962634ff72a756fb8d3ddb8e766d2eb373
Juan Felipe <98488909+jfrenza@users.noreply.github.com>
2025-04-16 19:16:47 -0500

    Actualizar exercise_1.py

--- class_3/exercises/exercise_1.py
+++ class_3/exercises/exercise_1.py
@@ -2,4 +2,4 @@
 
 Enunciado:
 Escribe una función que reciba una cadena de texto y devuelva la misma cadena
-convertida a mayúsculas. No uses print(), la función debe retornar el resultado."""
+convertida a mayúsculas. No uses print() dentro de la función. La función debe retornar el resultado."""
