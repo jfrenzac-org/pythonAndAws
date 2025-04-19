bdd6701ae646ca6d15a0f9eb47d6d3b4f1a367dd
EstebanAyalaC <ayalaesteban271@gmail.com>
2025-04-19 14:16:52 -0500

    Tarea1

--- class_1/exercises/exercise_1.py
+++ class_1/exercises/exercise_1.py
@@ -1,3 +1,20 @@
 # Write a program that asks the user for a number and classifies it as
 # positive, negative, or zero.
 
+
+numero=float(input("Escriba un número: "))
+
+if(numero==0):
+    print("El número es cero")
+elif(numero<0):
+    print("El número es negativo")
+else:
+    print("El número es positivo")
+
+    
+
+
+
+
+    
+
