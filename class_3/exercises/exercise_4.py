88066e1b452759d95b1bc9924e7ccb5e000ca1ff
miguelrenza <miguelrenza.internet@gmail.com>
2025-04-18 19:30:41 -0500

    EJERCICIOS_5_Y_4

--- class_3/exercises/exercise_4.py
+++ class_3/exercises/exercise_4.py
@@ -2,3 +2,42 @@
 Enunciado:
 Crea una función que reciba una frase (string) y devuelva una lista donde
 cada palabra de la frase sea un elemento, usando espacios como separadores."""
+
+frase = ("Estas son las palabras que forman esta frase.")
+
+def frase_en_lista(frase):
+
+    return list(frase.split())
+                
+lista_espaciada = frase.split("---")
+
+print(lista_espaciada)
+
+## palabras_frase = list(frase.split(dividir_frase_en_lista))
+
+##print(palabras_frase)
+
+#print(list(frase(-)))
+
+##if palabra in frase:
+##lista_palabras.append("-")
+
+##for palabra in (palabras_frase):
+    ##list(palabras_frase.append([]))
+    
+## print(lista_palabras)
+
+##def dividir_cadena_en_lista(cadena):
+"""
+    ##Convierte una cadena de texto en una lista donde cada palabra es un elemento.
+
+    Args:
+        cadena: La cadena de texto a dividir.
+
+    Returns:
+        Una lista donde cada palabra de la cadena es un elemento.
+    """
+   ## return cadena.split()
+
+
+
--- class_3/exercises/exercise_5.py
+++ class_3/exercises/exercise_5.py
@@ -5,3 +5,30 @@ Crea una función que reciba un texto largo y una palabra, y determine
 si esa palabra está contenida dentro del texto. La función debe devolver True o False.
 
 """
+texto = ("la palabra testigo debe estar contenida en el texto inicial, luego cada palabra que sea elegida "
+"por el usuario será evaluada y se definirá como falsa o verdadera en su pertenencia al texto.")
+
+palabra_elegida = input("Ingrese una palabra: ") 
+
+##lista_palabras = list(texto.split())
+## print(lista_palabras)
+
+def contiene_palabra_elegida (texto, palabra_elegida):
+    return palabra_elegida in texto
+
+resultado = contiene_palabra_elegida (texto.lower(), palabra_elegida.lower())
+if resultado == True:
+
+    print(resultado)
+
+    print(f"La palabra elegida es '{palabra_elegida}' y está contenida en el texto.")
+
+else:
+    print(resultado)
+    print(f"La palabra elegida es: '{palabra_elegida}'  y no está en el texto.")
+
+
+
+
+
+
