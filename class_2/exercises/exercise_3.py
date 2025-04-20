64ff0d48b9678351ea6383730b1a71af1d86625d
Eliana Chavarria <eliana.chavarriapino@postgrad.manchester.ac.uk>
2025-04-20 19:25:27 +0200

    exercise 2.3

--- class_2/exercises/exercise_3.py
+++ class_2/exercises/exercise_3.py
@@ -3,3 +3,10 @@ Write a program that takes a number as input and prints the multiplication
  table for that number from 1 to 10.
 
 Concepts Covered: for loops, input, print, range"""
+
+# Accept input from the user
+user_input = int(input("Enter a number: "))
+
+# Create iteration
+for i in range(1, 11):
+    print(i*user_input)
