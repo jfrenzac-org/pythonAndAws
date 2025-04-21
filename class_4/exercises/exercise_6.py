39e658ebd6ebdfac4326b4ac93f671dc3c09d07f
Juan Felipe <98488909+jfrenza@users.noreply.github.com>
2025-04-20 19:56:17 -0500

    Update exercise_6.py

--- class_4/exercises/exercise_6.py
+++ class_4/exercises/exercise_6.py
@@ -3,5 +3,5 @@ Multiplicar cada número por 10 solo si es mayor que 5
 Dada una lista de números, filtra aquellos mayores que 5 y multiplícalos por 10.
 Entrada: [2, 6, 8, 3, 1]
 Salida esperada: [60, 80]
-Usa filter() y luego map() con lambda.
+Usa filter(), map() y lambda.
 """
