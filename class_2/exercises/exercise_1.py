1f636c8fa82cf2b8f5df6ff7152d6b1b2e293c3c
EstebanAyalaC <ayalaesteban271@gmail.com>
2025-04-21 18:47:05 -0500

    Tarea2

--- class_2/exercises/exercise_1.py
+++ class_2/exercises/exercise_1.py
@@ -3,3 +3,17 @@ Create a program that accepts a list of numbers from the user (comma-separated) 
 sum of only the odd numbers in that list.
 
 Concepts Covered: Lists, for loops, input, print"""
+
+String=input("Escriba una lista de números separada por comas ")
+
+Lista=list(String.split(','))
+
+Suma=int(0);
+
+for i in range(len(Lista)):
+    
+    
+    if(Lista[i]!=","and int(Lista[i])%2!=0):
+      Suma=Suma+int(Lista[i])
+      
+print(Suma)
