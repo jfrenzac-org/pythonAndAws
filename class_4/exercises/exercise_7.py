1883ed2b150ac9ae47914703e7162ec81799d3a2
Juan Felipe <98488909+jfrenza@users.noreply.github.com>
2025-04-20 19:56:58 -0500

    Update exercise_7.py

--- class_4/exercises/exercise_7.py
+++ class_4/exercises/exercise_7.py
@@ -3,5 +3,5 @@ Capitalizar solo las palabras que tengan más de 4 letras
 De una lista de palabras, devolver otra con las que tienen más de 4 letras y estén capitalizadas.
 Entrada: ["sol", "computadora", "mesa", "ventana"]
 Salida esperada: ["Computadora", "Ventana"]
-Usa filter() y map() con lambda.
+Usa filter(), map() y lambda.
 """
