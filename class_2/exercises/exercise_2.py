33f63c16dd828448cb1ffbef35145cfe3d73928b
EstebanAyalaC <ayalaesteban271@gmail.com>
2025-04-21 18:49:42 -0500

    Tarea2

--- class_2/exercises/exercise_2.py
+++ class_2/exercises/exercise_2.py
@@ -3,3 +3,10 @@ Create a program that counts the occurrence of each word in a sentence
  entered by the user. It should store the words and their counts in a dictionary.
 
 Concepts Covered: Dictionaries, for loops, input, print, items()"""
+
+String=input("Escriba una frase ")
+
+Lista=list(String.split(' '))
+
+print(Lista)
+
