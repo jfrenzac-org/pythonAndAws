65c0d0c96f8538a1ea331434cab01cb27541b73d
EstebanAyalaC <ayalaesteban271@gmail.com>
2025-05-01 19:39:22 -0500

    Tarea2

--- class_2/exercises/exercise_4.py
+++ class_2/exercises/exercise_4.py
@@ -3,3 +3,31 @@ You are given two lists: one with keys and the other with values.
 Create a dictionary by pairing the elements of the two lists.
 
 Concepts Covered: Lists, dictionaries, for loops, zip(), items()"""
+
+
+Lista_keys=["abc","def","ghi","jkl","mno"]
+Lista_values=[25,32,456,987,6]
+Dict={}
+
+#Sin usar zip()
+
+def paring_list(Lista1,Lista2):
+
+    
+    if(len(Lista1)!=len(Lista2)):
+        print(f"Las listas nos son del mismo tamaño. Hay {len(Lista1)} elementos en keys y {len(Lista2)} en values.")
+
+    else:
+        for i in range(len(Lista1)):
+            Dict[Lista_keys[i]]=Lista_values[i]
+
+    return Dict
+
+    
+print(paring_list(Lista_keys,Lista_values))
+
+#Usando zip(). Aparea las listas hasta la posición que se apareable sin importar tamaño listas
+
+Dict=dict(zip(Lista_keys,Lista_values))
+print(Dict)
+
--- class_2/exercises/exercise_5.py
+++ class_2/exercises/exercise_5.py
@@ -4,3 +4,20 @@ the average of those values. The dictionary keys are student names, and the valu
  are their respective scores.
 
 Concepts Covered: Dictionaries, for loops, values(), sum(), len(), print"""
+
+
+Notas={"Juliet":4.5, "Lina":4.9, "Carlos":2.1, "Arturo":1.8, "Juan":0.2, "Simon":0.5, "Doris":3.7}
+
+#print(Notas)
+
+def get_average(Dict1):
+
+    suma=0
+    
+    for i,j in Dict1.items():
+        suma=suma+j
+
+    return(suma/len(Dict1))    
+
+print(f"El promedio de notas del salón es: {"%.2f" % round(get_average(Notas), 2)}")
+
